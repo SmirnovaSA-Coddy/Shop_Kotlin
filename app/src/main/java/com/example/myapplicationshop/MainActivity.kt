68d613cfa2aa523a1fae1cf5c1df7d4713ca7d86
Svetlana <smirns78@gmail.com>
2025-11-03 19:27:34 +0300

    New page, start page

--- app/src/main/java/com/example/myapplicationshop/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationshop/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myapplicationshop
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btn_Start = findViewById<Button>(R.id.btnStart)
+
+        btn_Start.setOnClickListener {
+//            Toast.makeText(this, "Кнопка нажата", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
+
+//        btn_Start.setOnLongClickListener {
+//            Toast.makeText(this, "Долгое нажатие", Toast.LENGTH_SHORT).show()
+//            true
+//        }
+
     }
 }
